2cafb327b0f0db09f914c50ff779507158068c72
Luke Sullivan <luke@sukelully.dev>
2025-05-28 18:12:19 +0100

    Implement submit button and education array iteration logic

--- src/components/CV/Education.jsx
+++ src/components/CV/Education.jsx
@@ -1,15 +1,29 @@
-import EducationComponent from './EducationComponent.jsx';
-
 export default function Education({ education }) {
   return (
     <div className="my-10 flex flex-col px-12">
       <h2 className="bg-neutral-200 text-sky-900 text-center font-semibold py-2">
         Education
       </h2>
-      {/* <EducationComponent education={education} /> */}
       {education.map((entry) => (
-        <EducationComponent education={entry} key={entry.id}/>
+        <EducationEntry key={entry.id} education={entry} />
       ))}
+    </div>
+  );
+}
+
+function EducationEntry({ education }) {
+  return (
+    <div className="flex items-center mt-4">
+      <div className="mr-12">
+        <span>{education['start-date']}</span>
+        &mdash;
+        <span>{education['end-date']}</span>
+      </div>
+
+      <div className="flex flex-col">
+        <p className="font-bold">{education.school}</p>
+        <p>{education.degree}</p>
+      </div>
     </div>
   );
 }
--- src/components/Form/EducationForm.jsx
+++ src/components/Form/EducationForm.jsx
@@ -1,16 +1,6 @@
 import FormComponent from './FormComponent';
 
 export default function EducationForm({ education, onChange }) {
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    const updatedEdu = {
-      ...education,
-      [name]: value,
-    };
-
-    onChange(updatedEdu);
-  };
-
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -19,7 +9,9 @@ export default function EducationForm({ education, onChange }) {
     formData.forEach((value, key) => {
       educationData[key] = value;
     });
-    console.log(educationData);
+    educationData.id = crypto.randomUUID();
+    
+    onChange([...education, educationData]);
   };
 
   return (
@@ -28,27 +20,21 @@ export default function EducationForm({ education, onChange }) {
       <FormComponent
         label="School"
         name="school"
-        value={education[0].school}
-        onChange={handleInputChange}
       />
       <FormComponent
         label="Degree"
         name="degree"
-        value={education[0].degree}
-        onChange={handleInputChange}
+        // value={education[0].degree}
+        // onChange={handleInputChange}
       />
       <div className="flex flex-wrap justify-between">
         <FormComponent
           label="Start"
           name="start-date"
-          value={education[0]['start-date']}
-          onChange={handleInputChange}
         />
         <FormComponent
           label="End"
           name="end-date"
-          value={education[0]['end-date']}
-          onChange={handleInputChange}
         />
       </div>
       <button className="cursor-pointer bg-sky-900 text-white font-semibold w-fit px-4 py-2 rounded-md ml-auto mr-4 mt-2" type="submit">
