3eeb6bc817863057d40cc00ebe84583a47163100
Luke Sullivan <luke@sukelully.dev>
2025-05-28 18:21:57 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # CV Application
 
-Odin Project [CV Application](https://www.theodinproject.com/lessons/node-path-react-new-cv-application)
+Simple CV generator.
--- src/components/Form/EducationForm.jsx
+++ src/components/Form/EducationForm.jsx
@@ -10,34 +10,26 @@ export default function EducationForm({ education, onChange }) {
       educationData[key] = value;
     });
     educationData.id = crypto.randomUUID();
-    
+
     onChange([...education, educationData]);
   };
 
   return (
-    <form className="flex flex-col bg-white rounded-md shadow-md p-6 gap-2" onSubmit={handleSubmit}>
+    <form
+      className="flex flex-col bg-white rounded-md shadow-md p-6 gap-2"
+      onSubmit={handleSubmit}
+    >
       <h1 className="font-bold text-3xl">Education</h1>
-      <FormComponent
-        label="School"
-        name="school"
-      />
-      <FormComponent
-        label="Degree"
-        name="degree"
-        // value={education[0].degree}
-        // onChange={handleInputChange}
-      />
+      <FormComponent label="School" name="school" />
+      <FormComponent label="Degree" name="degree" />
       <div className="flex flex-wrap justify-between">
-        <FormComponent
-          label="Start"
-          name="start-date"
-        />
-        <FormComponent
-          label="End"
-          name="end-date"
-        />
+        <FormComponent label="Start" name="start-date" />
+        <FormComponent label="End" name="end-date" />
       </div>
-      <button className="cursor-pointer bg-sky-900 text-white font-semibold w-fit px-4 py-2 rounded-md ml-auto mr-4 mt-2" type="submit">
+      <button
+        className="cursor-pointer bg-sky-900 text-white font-semibold w-fit px-4 py-2 rounded-md ml-auto mr-4 mt-2"
+        type="submit"
+      >
         Add education
       </button>
     </form>
