fa55c40feb5fbf479d4c76d20c44f1d17550ee80
Luke Sullivan <luke@sukelully.dev>
2025-05-30 15:55:02 +0100

    Begin experience form edit section

--- src/components/Form/ExperienceForm.jsx
+++ src/components/Form/ExperienceForm.jsx
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 import SubmitFormComponent from './FormComponent/SubmitFormComponent';
 
 export default function ExperienceForm({ experience, onChange }) {
+  const [editingEntry, setEditingEntry] = useState(null);
+
+  const handleEditEntry = (entry) => {
+    setEditingEntry(entry);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -20,6 +27,13 @@ export default function ExperienceForm({ experience, onChange }) {
   return (
     <div className="flex flex-col bg-white rounded-md shadow-md p-6 gap-2">
       <h1 className="font-bold text-3xl">Experience</h1>
+
+      <div id="form-entries">
+        {experience.map((entry) => (
+          <ExperienceFormEntry entry={entry} handleEditEntry={handleEditEntry} />
+        ))}
+      </div>
+
       <form className="flex flex-col" onSubmit={handleSubmit}>
         <SubmitFormComponent
           label="Company"
@@ -54,9 +68,22 @@ export default function ExperienceForm({ experience, onChange }) {
           className="cursor-pointer bg-sky-900 text-white font-semibold w-fit px-4 py-2 rounded-md ml-auto mr-4 mt-4"
           type="submit"
         >
-          Add experience
+          {editingEntry ? 'Update' : 'Add'} experience
         </button>
       </form>
+    </div>
+  );
+}
+
+function ExperienceFormEntry({ entry, handleEditEntry }) {
+  return (
+    <div className="flex gap-2">
+      <button
+        id={entry.id}
+        className="flex items-center w-full cursor-pointer bg-neutral-200 rounded-md p-2"
+      >
+        <span className="font-semibold">{entry.company}</span>
+      </button>
     </div>
   );
 }
